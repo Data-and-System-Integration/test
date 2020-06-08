03afbb09f53ff99b1ba13af6228279cd1542ab21
skedari <>
2020-06-08 06:53:24 -0400

    {/src/main/java/com/ashwini/test/test_dataObject.java}

--- src/main/java/com/ashwini/test/test_dataObject.java
+++ src/main/java/com/ashwini/test/test_dataObject.java
@@ -6,12 +6,44 @@ package com.ashwini.test;
 
 public class test_dataObject implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public test_dataObject() {
-    }
+	private java.lang.Integer field1;
+	private java.lang.String field2;
+	private java.lang.String field3;
 
+	public test_dataObject() {
+	}
 
+	public java.lang.Integer getField1() {
+		return this.field1;
+	}
 
+	public void setField1(java.lang.Integer field1) {
+		this.field1 = field1;
+	}
+
+	public java.lang.String getField2() {
+		return this.field2;
+	}
+
+	public void setField2(java.lang.String field2) {
+		this.field2 = field2;
+	}
+
+	public java.lang.String getField3() {
+		return this.field3;
+	}
+
+	public void setField3(java.lang.String field3) {
+		this.field3 = field3;
+	}
+
+	public test_dataObject(java.lang.Integer field1, java.lang.String field2,
+			java.lang.String field3) {
+		this.field1 = field1;
+		this.field2 = field2;
+		this.field3 = field3;
+	}
 
 }
